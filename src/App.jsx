160d1788923669679a272c3d1597ecef7d331e77
dariomedina10 <dariomedina10@gmail.com>
2025-09-19 12:29:04 -0400

    Cambios de Funcion

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,37 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import Todo from './Componentes/Todo'
+import Form from './Componentes/Form'
+import FilterButton from './Componentes/FilterButton'
 import './App.css'
-<App tasks={DATA} />
-const taskList = props.tasks?.map((task) => task.name);
-function App() {
 
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-       <div>
-
-<ul
-  role="list"
-  className="todo-list stack-large stack-exception"
-  aria-labelledby="list-heading">
-  
-  <Todo name="Eat" id="todo-0" completed />
-  <Todo name="Sleep" id="todo-1" />
-  <Todo name="Repeat" id="todo-2" />
-</ul>
-
-<ul
-  role="list"
-  className="todo-list stack-large stack-exception"
-  aria-labelledby="list-heading">
-  {taskList}
-</ul>
-           {/* <Todo/>
-             <Todo />
-          <Todo /> */}
-
-      {/*  <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-      </div> 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function retornarAleatorio() {
+  return Math.trunc(Math.random() * 10);
 }
 
-export default App
+function App() {
+  const siglo = 21
+  const persona = {
+    nombre: 'Juan',
+    edad: 34
+  }
+  return (
+    <div>
+      <h1>Título nivel 1</h1>
+      <hr />
+      <p>Estamos en el siglo {siglo}</p>
+      <h3>Acceso a un objeto</h3>
+      <p>{persona.nombre} tiene {persona.edad} años</p>
+      <h3>Llamada a un método</h3>
+      <p>Un valor aleatorio llamando a un método.</p>
+      {retornarAleatorio()}
+      <h3>Calculo inmediato de expresiones</h3>
+      3 + 3 = {3 + 3}
+    </div>
+
+    
+  );
+}
+
+
+export default App;
