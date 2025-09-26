4ecdbc31e5933fd43223c50251caa4b6baa51e4c
dariomedina10 <dariomedina10@gmail.com>
2025-09-26 11:23:52 -0400

    Cambios de Funcion II

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,34 @@ function retornarAleatorio() {
   return Math.trunc(Math.random() * 10);
 }
 
+function App2() {
+  return (
+    <div>
+      <form onSubmit={presion}>
+        <p>Ingrese primer valor:
+          <input type="number" name="valor1" />
+        </p>
+        <p>Ingrese segundo valor:
+          <input type="number" name="valor2" />
+        </p>
+        <p>
+          <input type="submit" value="Sumar" />
+        </p>
+      </form>
+    </div>
+  );
+}
+
+function presion(e) {
+  e.preventDefault();
+  const v1=parseInt(e.target.valor1.value, 10);
+  const v2=parseInt(e.target.valor2.value, 10);
+  const suma=v1+v2;
+  alert('La suma es:'+suma);
+}
+
+
+
 function App() {
   const siglo = 21
   const persona = {
@@ -28,8 +56,11 @@ function App() {
       {retornarAleatorio()}
       <h3>Calculo inmediato de expresiones</h3>
       3 + 3 = {3 + 3}
+      <App2 />;
     </div>
-
+   
+    
+   
     
   );
 }
